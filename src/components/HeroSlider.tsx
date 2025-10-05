13438a9620b17ec80c3d0240b284520a9b923920
Mustafa Hasanain <m7asanain@gmail.com>
2025-10-05 09:47:26 +0300

    feat: finalize Hero Slider component

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -5,36 +5,29 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import styles from "./HeroSlider.module.css";
 
 export default function HeroSlider() {
   const slides = [
     "/hero-sliders-img/hero-1.jpg",
     "/hero-sliders-img/hero-2.jpg",
     "/hero-sliders-img/hero-3.jpg",
+    "/hero-sliders-img/hero-4.jpg",
+    "/hero-sliders-img/hero-5.jpg",
+    "/hero-sliders-img/hero-6.jpg",
+    "/hero-sliders-img/hero-7.jpg",
+    "/hero-sliders-img/hero-8.jpg",
   ];
 
   return (
-    <div className="w-full h-full md:h-[700px] rounded-2xl overflow-hidden mb-10 mt-10">
+    <div className="w-full h-full md:h-[750px] rounded-2xl overflow-hidden mb-10 mt-5">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation
         pagination={{ clickable: true }}
         autoplay={{ delay: 5000 }}
         loop
-        className="w-full h-full rounded-2xl 
-\           [&_.swiper-button-prev]:!w-8
-          [&_.swiper-button-prev]:!h-8
-          [&_.swiper-button-next]:!w-8
-          [&_.swiper-button-next]:!h-8
-          [&_.swiper-button-prev]:!text-white
-          [&_.swiper-button-next]:!text-white
-          [&_.swiper-button-prev:after]:!text-lg
-          [&_.swiper-button-next:after]:!text-lg
-          [&_.swiper-button-prev]:!ml-5
-          [&_.swiper-button-next]:!mr-5
-          [&_.swiper-button-prev:hover]:!text-orange-500
-          [&_.swiper-button-next:hover]:!text-orange-500
-          "
+        className={styles.heroSlider}
       >
         {slides.map((src, index) => (
           <SwiperSlide key={index}>
